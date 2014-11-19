3c99906ed450274634657c3f5a4fb998c07559b7
Jacob Smallman <jacobsmallman89@gmail.com>
2014-11-19 13:26:08 -0500

    Update 2

---
+++ OrchestraApp/src/Ticket.java
@@ -0,0 +1,34 @@
+import java.sql.Date;
+
+
+public class Ticket {
+	
+	public Ticket(double price, String seatSection, char grade,
+			Date concertDate, Concert concert) {
+		super();
+		this.price = price;
+		this.seatSection = seatSection;
+		this.grade = grade;
+		this.concertDate = concertDate;
+		this.concert = concert;
+	}
+	private double price;
+	private String ticketID;
+	private String seatSection;
+	private char grade;
+	private Date concertDate;
+	private Concert concert;
+	
+	
+	
+	public double getPrice() {return price;}
+	public String getTicketID() {return ticketID;}
+	public String getSeatSection() {return seatSection;}
+	public char getGrade() {return grade;}
+	public Date getConcertDate() {return concertDate;}
+	public Concert getConcert() {return concert;}
+	
+	S
+	
+	
+}
